ec023499cad9c3e5497b6ef67706145a9d60fb39
Nicolas Bolle <nicolasbolle@gmail.com>
2020-05-27 22:16:43 -0400

    MORE DATA

--- chess_predictor/downloading.py
+++ chess_predictor/downloading.py
@@ -1,15 +1,9 @@
-# Redo: Buhmann, Nima_Junior, Wolverines1
-
 import pandas as pd
 import functions as f
 import lichess.api
 
-#leaders = ['Arnelos', 'EnglishLongbow', 'Dacii', 'K-Georgiev', 'vistagausta', 'may6enexttime', 'grizzlybear79', 'LazaroBruzon', 'BabaRamdev', 'ChermanTrawz', 'bushidonaruto', 'Bestinblitz', 'opperwezen', 'NathyGonzalez', 'Ernst_Gruenfeld', 'Federicov93', 'Chesser22', 'Brrrrrrrr', 'Hunni-7', 'IGMGataKamsky','Eduiturri','arturchix','NIndja64','Polyclinical', 'elcaseno', 'gsvc', 'pozvonochek', 'epistimonas', 'dalmatinac101', 'Samson_Ofubu', 'Sigma_Tauri', 'CrazySage', 'ultraking1', 'Tryhard00', 'RD4ever', 'Rakhmanov_Aleksandr', 'OpeningGeek', 'Drvitman', 'jsl', 'Last7Samurai', 'KasparovModeOn', 'MELND-777', 'S2Pac', 'Evgeny_Levin', 'Chepursias', 'Feokl1995', 'Kiborg1987', 'Yarebore', 'mutdpro', 'Arteler', 'sillyMASTER', 'recapture', 'A_Kukhmazov', 'Tayka', 'jitanu76', 'Abik02', 'GMVallejo', 'ChessWeeb', 'Stratersest', 'Grandelicious', 'DrunkGatineau', 'BartoszSocko', 'Inventing_Invention', 'KIBORG799', 'dmitrij_IM', 'AdriD', 'backrankissues', 'IVK88', 'justantan', 'GrazyHunter', 'Wolverines1', 'Shant7777777', 'Farrukh78', 'Nima_Junior', 'federovski', 'AbasovN', 'Dynamo37', 'VerdeNotte', 'CaptainJames25', 'Alexander_Donchenko', 'ElAsesinoFavorito', 'Brigello', 'GaMbit9285', 'darkghoul', 'Buhmann', 'Kelevra317', 'Amin_____tb', 'muisback', 'Alexandr_KhleBovich', 'lenochka18', 'GeorgMeier', 'IlSalvatore']
-leaders = ['backrankissues', 'IVK88', 'justantan', 'GrazyHunter']
+leaders = ['Yarebore', 'mutdpro', 'Arteler', 'sillyMASTER', 'recapture', 'A_Kukhmazov', 'Tayka', 'jitanu76', 'Abik02', 'GMVallejo', 'ChessWeeb', 'Stratersest', 'Grandelicious', 'DrunkGatineau', 'BartoszSocko', 'Inventing_Invention', 'KIBORG799', 'dmitrij_IM', 'AdriD']
 leaders.reverse()
-
-### Beginning of list that I've done before getting an error
-#['Konevlad', 'Liem_Chess', 'HansSchmidt', 'Zhigalko_Sergei', 'PUKLO', 'Benefactorr', 'MikeGScarn','E-Shaposhnikov']
 
 # Type of games'
 game_type = 'blitz'
